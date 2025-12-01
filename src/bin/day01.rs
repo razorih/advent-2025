1ae9cf2ac3c851bb42427a166d1d9a733385ada4
Jani Purhonen <razorih@users.noreply.github.com>
2025-12-01 13:15:11 +0200

    Day 01 - Gold

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -30,9 +30,52 @@ fn silver(input: &str) -> u32 {
     answer
 }
 
+fn gold(input: &str) -> u32 {
+    let mut answer = 0;
+    let mut dial: i32 = 50;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            break;
+        }
+
+        let (dir, count) = line.split_at(1);
+        let count = match count.parse::<i32>() {
+            Ok(num) => {
+                if dir == "L" {
+                    -num
+                } else {
+                    num
+                }
+            }
+            Err(_) => panic!(),
+        };
+
+        // from last iteration, "dial" is [0, 99] here
+        let next_dial = dial + count;
+
+        if next_dial >= 100 {
+            // positive rotation and crossed zero
+            answer += (next_dial / 100) as u32; // number of zero crossings
+        } else if next_dial <= 0 {
+            // negative rotation
+            // if dial was previously at 0 we don't want to count that as a zero crossing
+            if dial != 0 {
+                answer += 1;
+            }
+            answer += (next_dial.abs() / 100) as u32; // number of full rotations needed to add
+        }
+
+        dial = next_dial.rem_euclid(100);
+    }
+
+    answer
+}
+
 fn main() -> io::Result<()> {
     let input = read_input_from_env()?;
     println!("silver: {}", silver(&input));
+    println!("gold: {}", gold(&input));
 
     Ok(())
 }
