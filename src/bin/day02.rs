ba4560522be4e27577d4c9b846f8be697a7cb770
Jani Purhonen <razorih@users.noreply.github.com>
2025-12-02 11:53:42 +0200

    Day 02 - Gold

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -50,11 +50,47 @@ fn silver(input: &[Range]) -> u64 {
     sum
 }
 
+fn gold(input: &[Range]) -> u64 {
+    let mut sum = 0;
+    let mut buffer = String::new(); // buffer to hold formatted numbers
+
+    for range in input {
+        for num in range.start..=range.end {
+            buffer.clear();
+            write!(&mut buffer, "{}", num).unwrap();
+
+            // iteratively split the string into smaller and smaller chunks
+            // all the way into single digit and check if they are all the same
+            //
+            // for number 1212121212 iterations are
+            // 1. -> [12121, 21212] (no match, go on)
+            // 2. -> [1212, 1212, 1212] (match! break here)
+            for chunk_size in (1..(buffer.len() / 2) + 1).rev() {
+                let mut chunker = buffer.as_bytes().chunks_exact(chunk_size);
+                let first = chunker.next().unwrap();
+
+                // also after chunker has been exhausted, check if remainder was empty
+                // i.e. this number was evenly split
+                //
+                // if we tried 3093099 with chunk size 3
+                // -> [309, 309] (match!), but remainder = [9], so skip
+                if chunker.all(|chunk| chunk == first) && chunker.remainder().is_empty() {
+                    sum += num;
+                    break;
+                }
+            }
+        }
+    }
+
+    sum
+}
+
 fn main() -> io::Result<()> {
     let input = read_input_from_env()?;
     let input = parse(&input);
 
     println!("silver: {}", silver(&input));
+    println!("gold: {}", gold(&input));
 
     Ok(())
 }
