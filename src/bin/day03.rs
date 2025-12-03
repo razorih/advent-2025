78444bd8277172fbea5ed37a68ee9fa70bc666de
Jani Purhonen <razorih@users.noreply.github.com>
2025-12-03 21:38:12 +0200

    Day 03 - Optimizations
    - Skip scanning rest of the line when encountering a 9 - Reimplement
    calculating the final sum

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -8,7 +8,7 @@ fn solve<const N: usize>(banks: &Vec<Vec<u8>>) -> u64 {
 
 /// Calculate joltage of N batteries in given bank
 fn joltage<const N: usize>(bank: &[u8]) -> u64 {
-    let mut gathered: [u8; N] = [0; N];
+    let mut joltage = 0;
 
     // bank index where we can start looking for next maximum
     let mut cursor = 0;
@@ -22,19 +22,18 @@ fn joltage<const N: usize>(bank: &[u8]) -> u64 {
             if bank[i] > best {
                 best = bank[i];
                 cursor = i + 1;
+
+                if best == 9 {
+                    // cannot improve this number any more
+                    break;
+                }
             }
         }
 
-        gathered[n] = best;
+        joltage = joltage * 10 + best as u64;
     }
 
-    gathered
-        .into_iter()
-        .enumerate()
-        .fold(0_u64, |acc, (nth, digit)| {
-            let base = 10_u64.pow(N as u32 - nth as u32 - 1);
-            acc + (base * digit as u64)
-        })
+    joltage
 }
 
 fn main() -> io::Result<()> {
