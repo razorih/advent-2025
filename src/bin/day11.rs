d90781d028cb8ad7364b83b056a634bcd8f4b433
Jani Purhonen <razorih@users.noreply.github.com>
2025-12-11 17:07:37 +0200

    Day 11 - Gold

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,25 +1,57 @@
-use std::{collections::{HashMap, VecDeque}, io};
+use std::{collections::HashMap, io};
 
 use advent_2025::read_input_from_env;
 
 fn silver(graph: &HashMap<&str, Vec<&str>>) -> u64 {
-    let mut paths = 0;
+    count_paths(graph, "you", "out")
+}
 
-    // do a simple BFS search without breaking
-    let mut queue: VecDeque<&str> = VecDeque::new();
-    queue.push_back("you");
+fn gold(graph: &HashMap<&str, Vec<&str>>) -> u64 {
+    // since we're passing through fft and dac
+    // we can multiply unique paths
+    //   svr -> fft
+    //   fft -> dac
+    //   dac -> out
+    let svr_to_fft = count_paths(graph, "svr", "fft");
+    let fft_to_dac = count_paths(graph, "fft", "dac");
+    let dac_to_out = count_paths(graph, "dac", "out");
 
-    while let Some(current) = queue.pop_front() {
-        if current == "out" {
-            paths += 1;
-        }
+    // in reality there's only fft -> dac path, but verify
+    debug_assert_eq!(count_paths(graph, "dac", "fft"), 0);
 
-        if let Some(child_nodes) = graph.get(current) {
-            queue.extend(child_nodes);
-        }
+    svr_to_fft * fft_to_dac * dac_to_out
+}
+
+// https://stackoverflow.com/a/79295398
+fn recurse_paths<'a>(
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+    memo: &mut HashMap<&'a str, u64>,
+    from: &'a str,
+    to: &'a str,
+) -> u64 {
+    // base case
+    if from == to {
+        return 1;
     }
 
-    paths
+    // return if we already know how many paths exists to target from this node
+    if let Some(remembered) = memo.get(from) {
+        return *remembered;
+    }
+
+    let mut path_count = 0;
+    for child in graph.get(from).unwrap_or(&vec![]) {
+        path_count += recurse_paths(graph, memo, child, to);
+    }
+
+    memo.insert(from, path_count);
+    path_count
+}
+
+fn count_paths(graph: &HashMap<&str, Vec<&str>>, from: &str, to: &str) -> u64 {
+    let mut memo = HashMap::new();
+
+    recurse_paths(graph, &mut memo, from, to)
 }
 
 fn parse(input: &str) -> HashMap<&str, Vec<&str>> {
@@ -33,6 +65,25 @@ fn parse(input: &str) -> HashMap<&str, Vec<&str>> {
             Some((ins, outs))
         })
         .collect()
+}
+
+/// Transposes a graph, i.e. reverses all edges.
+/// Not needed
+#[allow(dead_code)]
+fn transpose_graph<'a>(
+    graph: &'a HashMap<&'a str, Vec<&'a str>>,
+) -> HashMap<&'a str, Vec<&'a str>> {
+    let mut out: HashMap<&str, Vec<&str>> = HashMap::with_capacity(graph.capacity());
+
+    for (&key, outs) in graph {
+        for &o in outs {
+            out.entry(o).or_default().push(key);
+        }
+    }
+
+    out.shrink_to_fit();
+
+    out
 }
 
 /// Visualize graph in Graphviz DOT language
@@ -50,6 +101,12 @@ fn to_graphviz(graph: &HashMap<&str, Vec<&str>>) -> String {
 
     out.push_str("you [shape=diamond, style=filled, fillcolor=cyan];\n");
     out.push_str("out [shape=dsquare, style=filled, fillcolor=red];\n");
+
+    // gold specific nodes
+    out.push_str("svr [shape=diamond, style=filled, fillcolor=green]\n");
+    out.push_str("dac [shape=diamond, style=filled, fillcolor=green]\n");
+    out.push_str("fft [shape=diamond, style=filled, fillcolor=green]\n");
+
     out.push_str("}");
 
     out
@@ -59,8 +116,12 @@ fn main() -> io::Result<()> {
     let input = read_input_from_env()?;
     let graph = parse(&input);
 
+    // let transpose = transpose_graph(&graph);
     // println!("{}", to_graphviz(&graph));
+    // println!("{}", to_graphviz(&transpose));
+
     println!("silver: {}", silver(&graph));
+    println!("gold: {}", gold(&graph));
 
     Ok(())
 }
